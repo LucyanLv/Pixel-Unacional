490310dbb108da15b4c29aa4884c39e5c94691cf
Lucyan <Levyathan666@gmail.com>
2023-07-08 11:35:38 -0500

    Arreglos botones
    Main menú completo, ahora funciona bien el botón de audio
    
    Pause menu funcionando completo en nivel 1 y nivel 2

--- Assets/Scripts/UI/AudioManager.cs
+++ Assets/Scripts/UI/AudioManager.cs
@@ -9,28 +9,36 @@ using Image = UnityEngine.UI.Image;
 public class AudioManager : MonoBehaviour
 {
     public static bool sound = true;
-    public Sprite soundOn;
-    public Sprite soundOff; 
+    //public Sprite soundOn;
+    //public Sprite soundOff;
+
+    [SerializeField] GameObject iconoSonido; 
+
+    
     public void SoundOn()
     {
-        gameObject.GetComponent<Image>().sprite = soundOn;
+        //gameObject.GetComponent<Image>().sprite = soundOn;
+        iconoSonido.GetComponent<Image>().color = Color.green;
         sound = true;
     }
     public void SoundOff() 
     {
-        gameObject.GetComponent<Image>().sprite = soundOff;
+       // gameObject.GetComponent<Image>().sprite = soundOff;
+        iconoSonido.GetComponent<Image>().color = Color.red;
         sound = false;
     }
     public void ChangeSound()
     {
         if (sound)
         {
-            gameObject.GetComponent<Image>().sprite = soundOff;
+           // gameObject.GetComponent<Image>().sprite = soundOff;
+            iconoSonido.GetComponent<Image>().color = Color.red;
             sound = false;
         }
         else
         {
-            gameObject.GetComponent<Image>().sprite = soundOn;
+           // gameObject.GetComponent<Image>().sprite = soundOn;
+            iconoSonido.GetComponent<Image>().color = Color.green;
             sound = true;
         }
     }
